eccac2e80ab1af9a06894003e7c538fcf0c3567d
caramel <AlexanderForward149@gmail.com>
2023-04-28 22:53:08 -0400

    finally

--- docs/memG.js
+++ docs/memG.js
@@ -1,149 +1,144 @@
-let score =0;
-const startButton = document.getElementById("startButton")
-const message = document.getElementById("message")
-const moves = document.getElementById("moveCounter");
-const timeValue = document.getElementById("time");
-const numberOfPics = 15;
-const imagePath = './images/';
-const imageExtension = '.jpg';
-const cards = [];
-let shuffledArray = [];
-let haveChosen = "false"
-let prevCards = ""
-let sec = 0
-let mins = 0
-//Initial moves and win count
-let movesCount = 0
+let moves = 0;
+let prvImg = false
+let nextImg = false
+let canFlip = true
 
-//Items array
-const items = [
-    { name: "1", image: "pic1.png" },
-    { name: "2", image: "pic2.png" },
-    { name: "3", image: "pic3.png" },
-    { name: "4", image: "pic4.png" },
-    { name: "5", image: "pic5.png" },
-    { name: "6", image: "pic6.png" },
-    { name: "7", image: "pic7.png" },
-    { name: "8", image: "pic8.png" },
-    { name: "9", image: "pic9.png" },
-    { name: "10", image: "pic10.png" },
-    { name: "11", image: "pic11.png" },
-    { name: "12", image: "pic12.png" },
-    { name: "13", image: "pic13.png" },
-    { name: "14", image: "pic14.png" },
-    { name: "15", image: "pic15.png" },
-  ];
+let timer
+let startTime = null;
+let timePassed = 0;
+
+const cardArea = document.getElementById('cardArea')
+const moveCounter = document.getElementById('moveCounter')
+const restartButton = document.getElementById('restartButton')
+const timeD = document.getElementById('time')
+const images = [
+    './images/pic1.jpg',    './images/pic1.jpg',
+    './images/pic2.jpg',    './images/pic2.jpg',
+    './images/pic3.jpg',    './images/pic3.jpg',
+    './images/pic4.jpg',    './images/pic4.jpg', 
+    './images/pic5.jpg',    './images/pic5.jpg',
+    './images/pic6.jpg',    './images/pic6.jpg',
+    './images/pic7.jpg',    './images/pic7.jpg',
+    './images/pic8.jpg',    './images/pic8.jpg',
+    './images/pic9.jpg',    './images/pic9.jpg',
+    './images/pic10.jpg',   './images/pic10.jpg',
+    './images/pic11.jpg',   './images/pic11.jpg',
+    './images/pic12.jpg',   './images/pic12.jpg',
+    './images/pic13.jpg',   './images/pic13.jpg',
+    './images/pic14.jpg',   './images/pic14.jpg',
+    './images/pic15.jpg',   './images/pic15.jpg',
+];
 
 
-function shuffler() {
-    // Loop through each picture and add it 2 times
-    for (let i = 1; i <= 15; i++) {
-      cards.push(`${imagePath}pic${i}${imageExtension}`);
-    }
-    // Shuffle 
-    const shuffledCards = cards.sort(() => Math.random() - 0.5);
-    console.log(shuffledCards);
-    console.log(cards)
-    return shuffledCards;
-  }
-  
-function insertImages() {
-        const imageTable = document.getElementById("imageTable");
-    const numRows = imageTable.rows.length;
-    const numCols = imageTable.rows[0].cells.length;
-  for (let i = 0; i < numRows; i++) {
-    for (let j = 0; j < numCols; j++) {
-        const img = document.createElement("img");
-        img.src = shuffledCards[cardIndex++];
-    }
-  }
+
+
+function startGame() {
+    const shuffledImages = shuffle(images)
+
+    shuffledImages.forEach(imagePath => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        card.image = imagePath
+
+        const img = document.createElement('img')
+        img.src = imagePath
+        img.style.display = 'none'
+        card.appendChild(img);
+        card.addEventListener('click', cardClick)
+        cardArea.appendChild(card)
+    });
+
+    moves = 0;
+    moveCounter.innerText = 'Moves: 0'
+    timePassed = 0;
+    timeD.innerText = 'Time: 0:00'
+    
+    timer = setInterval(() => {
+        elapsedTime = Date.now() - startTime;
+        const minutes = Math.floor(elapsedTime / 60000);
+        const seconds = Math.floor((elapsedTime % 60000) / 1000);
+        timeD.innerText = `Time: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    }, 1000);
 }
 
 
 
-// //Put them into the table
-// function insertImages() {
-//     const imageTable = document.getElementById("imageTable");
-//     const numRows = imageTable.rows.length;
-//     const numCols = imageTable.rows[0].cells.length;
-//     let cardIndex = 0;
-  
-//     for (let i = 0; i < numRows; i++) {
-//       for (let j = 0; j < numCols; j++) {
-//         const img = document.createElement("img");
-//         img.src = "./images/hidden.png";
-    
-//         const cell = document.createElement("td");
-//         cell.appendChild(img);
-//         imageTable.rows[i].appendChild(cell);
+function shuffle(cards) {
+    let cardI = cards.length;
+    let temporaryValue, randomIndex;
 
-//         // Reveal image on click
-//         cell.addEventListener('click', () => {
-//             const selectedImg = shuffledArray[cardIndex];
-//             console.log(`Previous img = ${prevCards}`)
-//             img.src = selectedImg;
-//             console.log(`Selected img = ${selectedImg}`)
-//             cardIndex++;
+    while (cardI !== 0) {
+        randomIndex = Math.floor(Math.random() * cardI);
+        cardI -= 1;
 
-//             // Compare images
-//             if (haveChosen == "false"){
-//                 prevCards = selectedImg
-//                 console.log(`Has Chosen: ${haveChosen} GOING IN`)
-//                 console.log(`Previous img = ${prevCards} FIRST CHOICE` )
-//                 console.log("")
-//                 message.innerText = "Select Another"
-//                 haveChosen = "true" 
-//                 console.log(`Has Chosen: ${haveChosen} COMING OUT`)
-//                 console.log("")
-//             }
-//             else if (prevCards === selectedImg) {
-//                     message.innerText = "You got a match!";
-
-//             } else if (!(prevCards === selectedImg)) {
-//                 message.innerText = "Try again";
-//                 console.log(`Has Chosen: ${haveChosen} GOING IN`)
-//                 prevCards = "";
-//                 console.log(`Previous img = ${prevCards} reset TRY AGAIN`)
-//                 haveChosen = "false"
-//                 console.log(`Has Chosen: ${haveChosen} COMING OUT`)
-
-//             }
-//         });
-
-//       }
-//     }
-// }
-  
-  
-// What happpans when you press the button
-
-
-function startGame(){
-    startButton.style.display = "none";
-    message.innerText = "GOOD LUCK";
-    
-    shuffledArray = shuffler();
-    insertImages();
-    
-
+        temporaryValue = cards[cardI];
+        cards[cardI] = cards[randomIndex];
+        cards[randomIndex] = temporaryValue;
+    }
+    return cards;
 }
 
-// const timeGenerator = () => {
-//     sec += 1;
-//     if (sec >= 60) {
-//       mins += 1;
-//       sec = 0;
-//     }
-//     //format time before displaying
-//     let secPass = sec < 10 ? `0${sec}` : sec;
-//     let minsPass = mins < 10 ? `0${mins}` : mins;
-//     timeValue.innerHTML = `<span>Time:</span>${minsPass}:${secPass}`;
-//   };
+//shows the cards
+function cardClick(event) {
+    if (!canFlip || this.classList.contains('shown') || this.classList.contains('matched')) {
+        return;
+    }
+    this.classList.add('shown');
+    this.querySelector('img').style.display = 'block'
+        console.log(`Previous is ${prvImg}`)
 
-//   const movesCounter = () => {
-//     movesCount += 1;
-//     moves.innerHTML = `Moves:${movesCount}`;
-//   };
+        if (!startTime) {
+            startTime = Date.now();
+            setInterval(function() {
+              elapsedTime = Date.now() - startTime;
+              const minutes = Math.floor(elapsedTime / 60000);
+              const seconds = Math.floor((elapsedTime % 60000) / 1000)
+              timeD.innerText = `Time: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+            }, 1000);
+          }
 
-// Start button event listener 
-startButton.addEventListener("click", startGame);
+    if (prvImg ==false) {
+        console.log(`Previous is ${prvImg}`)
+        prvImg = this;
+        console.log(`Previous is ${prvImg}`)
+    } else if (!nextImg) { 
+        nextImg = this;
+        moves = moves + 1;
+        moveCounter.innerText = `Moves: ${moves}`
+
+        if (prvImg.image === nextImg.image) {
+            prvImg.classList.add('matched')
+            nextImg.classList.add('matched')
+            prvImg = false
+            nextImg = false;
+
+            const unmatchedCards = document.querySelectorAll('.card:not(.matched)')
+        if (unmatchedCards.length === 0) {
+            clearInterval(timer)
+            setTimeout(() => {
+            cardArea.innerHTML = '<h1>You have won the game!</h1>'
+            restartButton.innerText ="Restart Game"
+    }, 1000);
+}
+        } else {
+            canFlip = false;
+            setTimeout(() => {
+                prvImg.classList.remove('shown');
+                nextImg.classList.remove('shown');
+                prvImg.querySelector('img').style.display = 'none'
+                nextImg.querySelector('img').style.display = 'none'
+                prvImg = false
+                nextImg = false
+                canFlip = true
+            }, 1500);
+        }
+    }
+}
+
+function restartGame() {
+    clearInterval(timer)
+    startTime = null;
+    cardArea.innerHTML = ''
+    startGame()
+}
+restartButton.addEventListener('click', restartGame)
